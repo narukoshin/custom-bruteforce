2c084d0e2938c0d686560c9d8c9675e9b8d1e690
Naru Koshin <naru.koshin@outlook.jp>
2021-12-24 20:43:37 +0200

    added a lot of checkings, error messages and more cleaner code

--- pkg/bruteforce/bruteforce.go
+++ pkg/bruteforce/bruteforce.go
@@ -3,10 +3,9 @@ package bruteforce
 import (
 	"custom-bruteforce/pkg/config"
 	"custom-bruteforce/pkg/structs"
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
-	"os"
 	"strings"
 )
 
@@ -14,29 +13,67 @@ var Types_Available []string = []string{"list", "file"}
 
 var (
 	Field 	string 		= config.YAMLConfig.B.Field
-	Type  	string 		= config.YAMLConfig.B.Type
-	Source	string 		= config.YAMLConfig.B.Source
+	From  	string 		= config.YAMLConfig.B.From
+	File	string 		= config.YAMLConfig.B.File
 	List	[]string	= config.YAMLConfig.B.List
 	Fail	structs.YAMLOn_fail = config.YAMLConfig.OF
 )
 
-func Dictionary() []string {
-	if Type == "list" {
-		if len(List) == 0 {
-			fmt.Println("Please add passwords to the list")
-			os.Exit(0)
+// adding some error messages
+var ErrNoPasswords = errors.New("there is no passwords available for bruteforce, please specify some passwords")
+var ErrOpeningFIle = errors.New("we have issues with opening a file, make sure that file exists and is readable")
+var ErrWrongType   = errors.New("you specified the wrong source of dictionary, allowed types are (file, list)")
+
+func verify_type() bool{
+	for _, t := range Types_Available {
+		if From == t {
+			return true
 		}
-		return List
-	} else if Type == "file" {
-		if _, err := os.Stat(Source); os.IsNotExist(err) {
-			fmt.Printf("File %s does not exist\n", Source)
-			os.Exit(0)
-		}
-		contents, err := ioutil.ReadFile(Source)
-		if err != nil {
-			log.Fatal(err)
-		}
-		return strings.Split(string(contents), "\n")
 	}
-	return []string{}
+	return false
+}
+
+// Loading dictionary from file or from the list that is defined in the config file
+func Dictionary() ([]string, error) {
+	var wordlist []string
+	// Verifying that the source of the dictionary is allowed
+	if ok := verify_type(); !ok {
+		return nil, ErrWrongType
+	}
+	if From == "list" {
+		wordlist = List
+	} else if From == "file" {
+		contents, err := ioutil.ReadFile(File)
+		if err != nil {
+			return nil, ErrOpeningFIle
+		}
+		wordlist = strings.Split(string(contents), "\n")
+	}
+	// Checking if the last element of the list is not empty
+	// If the last element is empty, then we are deleting it 
+	{
+		if len(wordlist) != 0 {
+			if len(wordlist[len(wordlist)-1]) == 0 {
+				wordlist = wordlist[:len(wordlist)-1]
+			}
+		}
+	}
+	// When we cleared empty line at the end, let's check if the list is not empty now
+	if len(wordlist) == 0 {
+		return nil, ErrNoPasswords
+	}	
+	return wordlist, nil
+}
+
+// The function where all the magic happens
+func Start() error {
+	// loading wordlist
+	wordlist, err := Dictionary()
+	if err != nil {
+		return err
+	}
+	for _, password := range wordlist {
+		fmt.Println(password)
+	}
+	return nil
 }
